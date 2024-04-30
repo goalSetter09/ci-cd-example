3271f2d826da6549f712082ab2406efbdb937600
dongyoung <dong0579@naver.com>
2024-04-30 19:18:09 +0900

    first commit

---
+++ src/main/java/com/example/ciciexample/CiCiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ciciexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCiExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ciciexample/CiCiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ciciexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
